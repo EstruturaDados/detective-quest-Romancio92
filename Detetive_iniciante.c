d3c53c5e6d9ae776683ca9ec4b3b8ab4c5c1523f
Romancio92 <romuloamancio92@gmail.com>
2025-11-13 18:21:20 +0000

    versao 1

---
+++ Detetive_iniciante.c
@@ -0,0 +1,15 @@
+
+//inclusão de bibliotecas.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//inicialização da função main.
+int main 
+{
+
+
+    return 0;
+}
+
+//definição de funções.
