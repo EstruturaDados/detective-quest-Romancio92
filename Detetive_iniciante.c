5a1886be54e818b83eec4b4303c320fbfeda62fd
Romancio92 <romuloamancio92@gmail.com>
2025-11-19 23:45:35 +0000

    Versao 6

--- Detetive_iniciante.c
+++ Detetive_iniciante.c
@@ -7,109 +7,117 @@
 typedef struct No
 {
     char valor[50];
-    struct No* esquerda;
-    struct No* direita;
+        struct No* esquerda;
+            struct No* direita;
 
-} No;
+            } No;
 
-//inicialização da função main.
-int main ()
-{
-    No *raiz = NULL;
+            No* criarNo (const char* valor);
+            No* inserir(No* raiz, const char*valor);
+            void preOrdem (No *raiz);
+            void emOrdem (No *raiz);
+            void posOrdem (No *raiz);
+            void liberar(No *raiz);
 
-    raiz = inserir(raiz, "Hall de Entrada");
-    raiz = inserir(raiz, "Sala de Estar");
-    raiz = inserir(raiz, "Biblioteca");
-    raiz = inserir(raiz, "Quarto");
+            //inicialização da função main.
+            int main ()
+            {
+                No *raiz = NULL;
 
-    printf("Pre-ordem: ");
-    preOrdem(raiz);
-    printf(\n);
+                    raiz = inserir(raiz, "Hall de Entrada");
+                        raiz = inserir(raiz, "Sala de Estar");
+                            raiz = inserir(raiz, "Biblioteca");
+                                raiz = inserir(raiz, "Quarto");
 
-    printf("Em ordem: ");
-    emOrdem(raiz);
-    printf(\n);
+                                    printf("Pre-ordem: ");
+                                        preOrdem(raiz);
+                                            printf("\n");
 
-    printf("Pos ordem: ");
-    posOrdem(raiz);
-    printf(\n);
+                                                printf("Em ordem: ");
+                                                    emOrdem(raiz);
+                                                        printf("\n");
 
-    liberar(raiz);
+                                                            printf("Pos ordem: ");
+                                                                posOrdem(raiz);
+                                                                    printf("\n");
 
-    return 0;
-}
+                                                                        liberar(raiz);
 
-//definição de funções.
-//definição de função para criar nó.
-No* criarNo (const char* valor)
-{
-    No* novo =  (No*) malloc(sizeof(No));
-    if (novo == NULL)
-    {
-        printf("Erro ao alocar memória!\n");
-        exit(1);
-    }
-    strcpy(novo->valor, valor);
-    novo->esquerda = NULL;
-    novo->direita = NULL;
-    return novo;
-}
+                                                                            return 0;
+                                                                            }
 
-//criar função de inserir nó.
-No* inserir(No* raiz, const char*valor)
-{
-    if (raiz == NULL)
-    {
-        return criarNo(valor);
-    }
-    if (strcmp(valor, raiz->valor) < 0);
-    {
-        raiz->esquerda = inserir(raiz->esquerda, valor);
-    }
-    else
-    {
-        raiz->direita = inserir(raiz->direita, valor);
-    }
-    return raiz;
-}
+                                                                            //definição de funções.
+                                                                            //definição de função para criar nó.
+                                                                            No* criarNo (const char* valor)
+                                                                            {
+                                                                                No* novo =  (No*) malloc(sizeof(No));
+                                                                                    if (novo == NULL)
+                                                                                        {
+                                                                                                printf("Erro ao alocar memória!\n");
+                                                                                                        exit(1);
+                                                                                                            }
+                                                                                                                strcpy(novo->valor, valor);
+                                                                                                                    novo->esquerda = NULL;
+                                                                                                                        novo->direita = NULL;
+                                                                                                                            return novo;
+                                                                                                                            }
 
-void preOrdem (No *raiz)
-{
-    if (raiz != NULL)
-    {
-        printf("%s", raiz->valor);
-        preOrdem(raiz->esquerda);
-        preOrdem(raiz->direita);
-    }
-}
+                                                                                                                            //criar função de inserir nó.
+                                                                                                                            No* inserir(No* raiz, const char*valor)
+                                                                                                                            {
+                                                                                                                                if (raiz == NULL)
+                                                                                                                                    {
+                                                                                                                                            return criarNo(valor);
+                                                                                                                                                }
 
-void emOrdem (No *raiz)
-{
-    if (raiz != NULL)
-    {
-        emOrdem(raiz->esquerda);
-        printf("%s", raiz->valor);
-        preOrdem(raiz->direita);
-    }
-}
+                                                                                                                                                    if (strcmp(valor, raiz->valor) < 0)
+                                                                                                                                                        {
+                                                                                                                                                                raiz->esquerda = inserir(raiz->esquerda, valor);
+                                                                                                                                                                    }
+                                                                                                                                                                        else
+                                                                                                                                                                            {
+                                                                                                                                                                                    raiz->direita = inserir(raiz->direita, valor);
+                                                                                                                                                                                        }
+                                                                                                                                                                                            return raiz;
+                                                                                                                                                                                            }
 
-void posOrdem (No *raiz)
-{
-    if (raiz != NULL)
-    {
-        emOrdem(raiz->esquerda);
-        preOrdem(raiz->direita);
-        printf("%s", raiz->valor);
-    }
-}
+                                                                                                                                                                                            void preOrdem (No *raiz)
+                                                                                                                                                                                            {
+                                                                                                                                                                                                if (raiz != NULL)
+                                                                                                                                                                                                    {
+                                                                                                                                                                                                            printf("%s", raiz->valor);
+                                                                                                                                                                                                                    preOrdem(raiz->esquerda);
+                                                                                                                                                                                                                            preOrdem(raiz->direita);
+                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                }
 
-void liberar(No *raiz)
-{
-    if (raiz != NULL)
-    {
-        liberar(raiz->esquerda);
-        liberar(raiz->direita);
-        free(raiz);
-    }
-}
+                                                                                                                                                                                                                                void emOrdem (No *raiz)
+                                                                                                                                                                                                                                {
+                                                                                                                                                                                                                                    if (raiz != NULL)
+                                                                                                                                                                                                                                        {
+                                                                                                                                                                                                                                                emOrdem(raiz->esquerda);
+                                                                                                                                                                                                                                                        printf("%s", raiz->valor);
+                                                                                                                                                                                                                                                                preOrdem(raiz->direita);
+                                                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                                                    }
+
+                                                                                                                                                                                                                                                                    void posOrdem (No *raiz)
+                                                                                                                                                                                                                                                                    {
+                                                                                                                                                                                                                                                                        if (raiz != NULL)
+                                                                                                                                                                                                                                                                            {
+                                                                                                                                                                                                                                                                                    emOrdem(raiz->esquerda);
+                                                                                                                                                                                                                                                                                            preOrdem(raiz->direita);
+                                                                                                                                                                                                                                                                                                    printf("%s", raiz->valor);
+                                                                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                                                                        }
+
+                                                                                                                                                                                                                                                                                                        void liberar(No *raiz)
+                                                                                                                                                                                                                                                                                                        {
+                                                                                                                                                                                                                                                                                                            if (raiz != NULL)
+                                                                                                                                                                                                                                                                                                                {
+                                                                                                                                                                                                                                                                                                                        liberar(raiz->esquerda);
+                                                                                                                                                                                                                                                                                                                                liberar(raiz->direita);
+                                                                                                                                                                                                                                                                                                                                        free(raiz);
+                                                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                                                            }
 
